0f04eb8132fabc5a5d800fd5ecaa0a3f9e3b3b31
Soumaya Zirari <soumaya.zirari@gmail.com>
2015-10-08 16:53:40 +0200

    using and namespace

--- ConsoleApplication1/m2i.DotNet/Program.cs
+++ ConsoleApplication1/m2i.DotNet/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using m2i.DotNEt;
 
-namespace ConsoleApplication1
+namespace m2i.DotNEt
 {
     class Program
     {
