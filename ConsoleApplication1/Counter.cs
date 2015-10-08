d7000988c23a8602c00c3985a62f1505589f0073
Soumaya Zirari <soumaya.zirari@gmail.com>
2015-10-08 16:42:57 +0200

    commit2 .Net

---
+++ ConsoleApplication1/Counter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Counter
+    {
+        private static int i = 0;
+   
+
+    public static int increment() { return i++; }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,12 +10,35 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Class2 test = new Class2();
+            Book book1 = new Book();
+            Book book2 = new Book();
+            Author author = new Author ();
+            Publisher p = new Publisher();
 
-            test.Xxx = 3;
+            author.FirstName = "test";
+            author.LastName = "test";
+            book1.addAuthor(author);
+            book1.Title = "Book ";
+            book1.Category = BookCategory.Computer;
 
-            Console.WriteLine(test.Xxx);
-            Console.ReadKey();
+            p.Name="toto";
+            p.Id = 12;
+            book1.addPublisher(p);
+
+
+
+            // book1.DateSortie = 12;
+           Console.WriteLine(book1.Category);
+           Console.ReadKey();
+            //book2.addAuthor(author);
+            //book2.Title = "Book ";
+            //if (book1.Author.LastName == book2.Author.LastName)
+            //  Console.WriteLine("OK !");
+            //else
+            //  Console.WriteLine("Deux objets sont forcement differents");
+
+        
+
         }
     }
 
